2639134209e1e196e38d642ccb38fa2bf9245bf6
StrickVax <strickvax@gmail.com>
2022-03-11 00:55:32 -0800

    More stuff

--- Chapter_3.py
+++ Chapter_3.py
@@ -1,3 +1,20 @@
 # Comment. Nice.
 a = 2 + 2
 print(a)
+
+# Division
+b = 52 / 3
+print(round(b,2))
+
+# Floor division
+b = 51 // 4
+print(round(b,2))
+
+# Square
+print(5 ** 2)
+
+# Hmm
+width = 50
+height = 30
+area = width * height
+print(area)
