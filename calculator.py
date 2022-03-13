6f3644b63672dcdd788b77f15f34c32b4f4c10ae
StrickVax <strickvax@gmail.com>
2022-03-12 17:45:57 -0800

    Added ability to operate on 3+ numbers

--- calculator.py
+++ calculator.py
@@ -1,9 +1,5 @@
 # This app can take multiple numbers and output their calculations
 
-x = float(input())
-o = str(input())
-y = float(input())
-
 
 def add(x, y):
     return x + y
@@ -25,19 +21,52 @@ def pow(x, y):
     return x**y
 
 
+# Gets two numbers and the operator, and returns the sum
+def calculator():
+    x = float(input("FN "))
+    o = str(input("O  "))
+
+    # If user inputs "=", the program will just return the sum
+    if o == "=":
+        return operation(o, x, y=0)
+
+    while o != "=":
+        y = float(input("SN"))
+        x = operation(o, x, y)
+
+        print("sum: ", x)
+        o = str(input("O2 "))
+        y = float(input())
+
+    return operation(o, x, y)
+
+
 # TO-DO: Take multiple entries
-def operation(f):
+# Takes two numbers and the operator. It then prints the calculation (x _ y = sum), before returning the sum
+def operation(f, x, y):
     match f:
-        case ("+"):
-            print(x, "+", y, "=", add(x, y))
-        case ("-"):
-            print(x, "-", y, "=", subtract(x, y))
-        case ("*"):
-            print(x, "*", y, "=", multiply(x, y))
-        case ("/"):
-            print(x, "/", y, "=", divide(x, y))
-        case ("^"):
-            print(x, "^", y, "=", pow(x, y))
+        case "+":
+            print(x, "+", y, "=", end=" ")
+            return add(x, y)
+        case "-":
+            print(x, "-", y, "=", end=" ")
+            return subtract(x, y)
+        case "*":
+            print(x, "*", y, "=", end=" ")
+            return multiply(x, y)
+        case "/":
+            print(x, "/", y, "=", end=" ")
+            return divide(x, y)
+        case "^":
+            print(x, "^", y, "=", end=" ")
+            return pow(x, y)
+        case _:
+            print(x, "=", end=" ")
+            return x
 
 
-operation(o)
+flag = 1
+
+while flag != 0:
+    print(calculator())
+    flag = float(input("Continue? (0) "))
