eac55407c8b85e23c3529e9185bfb5d9c8312e53
StrickVax <strickvax@gmail.com>
2022-03-13 22:51:30 -0700

    Appends and lists

--- Chapter_3.py
+++ Chapter_3.py
@@ -25,7 +25,38 @@ print(s, "kkk")
 
 # r for raaaw
 print(r"c:\\hm")
-print("\n\nc:\nm")
+print("\nc:\nm")
 
 # A
 print(s)
+
+print(s * 3)
+
+print(
+    """\
+        Holy cow
+    Lois
+    """
+)
+
+print("Sa" "ns")
+
+print()
+print()
+print()
+
+z = "Python"
+
+print(z[-4:])
+
+y = z[:3] + "AAA"
+print(y)
+print(len(y))
+
+squares = [1, 8, 64, 125]
+squares[1] = 9999
+print(squares[:])
+
+squares = [1, 3, 4]
+squares.append(99)
+print(squares)
